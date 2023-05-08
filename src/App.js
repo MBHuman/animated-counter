b5e97e43155b2ce1a56d4fc6a7de5e8c34c75091
Evgeny <zenka712@mail.ru>
2023-05-08 15:19:43 +0300

    add animated counter

--- src/App.js
+++ src/App.js
@@ -1,57 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
-import './App.css';
+import './App.scss';
+import Quote from './features/quote/Quote';
+import { Row } from 'react-bootstrap';
+import TestAnimation from './features/quote/TestAnimation';
 
 function App() {
   return (
-    <div className="App">
+    <Row className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <Quote />
       </header>
-    </div>
+    </Row>
   );
 }
 
